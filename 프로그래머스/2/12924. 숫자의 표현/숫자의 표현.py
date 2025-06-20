a9903916b828822847732126657b4f2fda179097
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2025-06-20 20:36:42 +0900

    [level 2] Title: 숫자의 표현, Time: 0.78 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,13 +1,15 @@
 def solution(n):
     answer = 0
-    k = 1
-    while k <= n:
-        sum_dummy = 0
-        for i in range(k,n+1):
-            sum_dummy += i
-            if sum_dummy >= n: break
-        if sum_dummy == n:
-            answer += 1
-        k += 1
-        
+    for i in range(1,n+1):
+        # 등차수열 합 공식
+        # a는 등차수열을 시작하는 수 i는 총 몇개를 더할지
+        # 2n = i(2a + i - 1) -> 2n/i = 2a + i - 1 -> 2n/i - i + 1 = 2a
+        # 2n이 i로 나뉘어떨어져야 합공식을 충족함
+        if (2 * n) % i != 0: continue
+        # a = (2n/i - i + 1)/2 -> (2n/i)/2 - (i - 1)/2
+        temp = (2 * n) // i - i + 1
+        if temp > 0 and temp % 2 == 0:
+            a = temp // 2
+            if a > 0:
+                answer += 1
     return answer

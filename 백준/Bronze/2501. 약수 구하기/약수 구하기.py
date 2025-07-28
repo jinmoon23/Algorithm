a9009feb3ac759b3736a734fb65925cdda860482
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2025-07-28 13:25:21 +0900

    [Bronze III] Title: 약수 구하기, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,8 @@
+N, K = map(int, input().split())
+divider_list = []
+# 1. 1부터 N까지의 값을 N과 나머지 연산함
+for i in range(1, N+1):
+    # 2. 약수를 준비한 리스트에 인입
+    if N % i == 0:
+        divider_list.append(i)
+print(divider_list[K-1] if len(divider_list) >= K else 0)

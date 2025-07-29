5474c30b03313413e9f180f03780098153cdfd0c
최진문 <wlsansdlek1@me.com>
2025-07-29 13:51:56 +0900

    [백준] 최소,최대 practice

---
+++ Practice/백준/최소, 최대/sol.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open("input.txt")
+
+N = int(input())
+numbers = list(map(int, input().split()))
+min_num = numbers[0]
+max_num = numbers[0]
+for i in range(N):
+    if numbers[i] < min_num:
+        min_num = numbers[i]
+    if numbers[i] > max_num:
+        max_num = numbers[i]
+print(min_num, max_num)

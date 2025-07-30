3857fbfcb2f4ca4b820d87728d8ecb2185227114
최진문 <wlsansdlek1@me.com>
2025-07-30 11:00:02 +0900

    [백준] 피보나치수5 practice

---
+++ Practice/백준/피보나치수5/sol.py
@@ -0,0 +1,8 @@
+import sys
+sys.stdin = open("input.txt")
+
+N = int(input())
+fivo = [0, 1]
+for i in range(2, N+1):
+    fivo.append(fivo[i - 1] + fivo[i - 2])
+print(fivo[N])
---
+++ Practice/백준/피보나치수5/sol_refactor.py
@@ -0,0 +1,8 @@
+import sys
+sys.stdin = open("input.txt")
+
+N = int(input())
+a, b = 0, 1
+for _ in range(N):
+    a, b = b, a + b
+print(a)

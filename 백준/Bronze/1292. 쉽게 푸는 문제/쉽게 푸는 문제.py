c018ebece6b4cdff4a986a2f5b24945056bafc3a
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2025-08-01 10:54:30 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.py
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.py
@@ -3,5 +3,7 @@ arr = []
 for i in range(1, 1001):
     for j in range(i):
         arr.append(i)
+        if len(arr) == M: break
+    if len(arr) == M: break
 answer = sum(arr[N-1:M])
 print(answer)

2cd5c36d09d3035161da67729f99c5dbb5a14b4e
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2025-11-14 20:39:21 +0900

    [level 1] Title: 모의고사, Time: 27.32 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=javascript) 
 
 ### 성능 요약
 
-메모리: 9.36 MB, 시간: 5.81 ms
+메모리: 38.4 MB, 시간: 27.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 08일 12:52:07
+2025년 11월 14일 20:39:15
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -0,0 +1,27 @@
+function solution(answers) {
+    const patterns = [
+        [1,2,3,4,5],
+        [2,1,2,3,2,4,2,5],
+        [3,3,1,1,2,2,4,4,5,5,]
+    ];
+    
+    const scores = [0, 0, 0];
+    
+    for (const [i, answer] of answers.entries()) {
+        for (const [j, pattern] of patterns.entries()) {
+            if (answer === pattern[i % pattern.length]) {
+                scores[j] += 1;
+            }
+        }
+    }
+    const maxScore = Math.max(...scores);
+    
+    const answer = []
+    
+    for (let [k, score] of scores.entries()) {
+        if (score === maxScore) {
+            answer.push(k + 1);
+        }
+    }
+    return answer;
+}

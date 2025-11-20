c547af8d84a4d102aa62d80f4074db6b12a4f68d
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2025-11-20 16:49:32 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -0,0 +1,12 @@
+-- 코드를 입력하세요
+-- 입양을 간 동물 중, 보호 기간이 가장 길었던 동물 두 마리
+-- 보호 기간 = O.DATETIME - I.DATETIME
+SELECT
+    O.ANIMAL_ID, O.NAME
+FROM
+    ANIMAL_OUTS O
+JOIN 
+    ANIMAL_INS I ON O.ANIMAL_ID = I.ANIMAL_ID
+ORDER BY
+     O.DATETIME - I.DATETIME DESC
+LIMIT 2;

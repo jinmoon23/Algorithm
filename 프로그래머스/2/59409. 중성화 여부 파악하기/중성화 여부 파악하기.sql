a019ab7f07ca6dc43851728467e0175fe42ce711
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2025-11-26 09:13:40 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+SELECT
+    ANIMAL_ID, NAME,
+    CASE WHEN SEX_UPON_INTAKE IN ('Spayed Female', 'Neutered Male') THEN 'O'
+    ELSE 'X'
+    END AS 중성화
+FROM
+    ANIMAL_INS
+ORDER BY
+    ANIMAL_ID;

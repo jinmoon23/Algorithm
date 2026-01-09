87bc0f803bc97071162d618eaebc36b9d3cd83a3
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2026-01-09 10:23:33 +0900

    [level 1] Title: 카드 뭉치, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,14 +1,11 @@
 function solution(cards1, cards2, goal) {
-    let answer = 'Yes';
-    while (goal.length > 0) {
-        const macher = goal.shift();
-        if (macher == cards1[0]) {
+    while(goal.length > 0) {
+        const currentCard = goal.shift();
+        if (currentCard == cards1[0]) {
             cards1.shift();
-        } else if (macher == cards2[0]) {
+        } else if (currentCard == cards2[0]) {
             cards2.shift();
-        } else {
-            answer = "No"
-        }
+        } else return "No"
     }
-    return answer;
+    return "Yes"
 }

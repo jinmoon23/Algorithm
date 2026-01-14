3846754aa41bd6f88773c768a1d8fdd7a6e1a806
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2026-01-15 08:58:03 +0900

    [level 3] Title: 베스트앨범, Time: 5.82 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -2,32 +2,37 @@
 
 function solution(genres, plays) {
     var answer = [];
+    const genresPlaySum = {};
     const songs = [];
-    const genreToSum = {};
+    // 1. 각 장르의 총 재생횟수와 장르에 속한 노래의 재생횟수를 담은 오브젝트와 배열 파싱
     for (let i = 0; i < genres.length; i++) {
         songs.push({genre: genres[i], play: plays[i], id: i});
-        if (genreToSum[genres[i]]) {
-            genreToSum[genres[i]] += plays[i];            
+        if (genresPlaySum[genres[i]]) {
+            genresPlaySum[genres[i]] += plays[i];
         } else {
-            genreToSum[genres[i]] = plays[i];
+            genresPlaySum[genres[i]] = plays[i];
         }
     }
-    const sortedGenres = Object.keys(genreToSum).sort((a,b) => {
-        return genreToSum[b] - genreToSum[a];
+    console.log(genresPlaySum, songs);
+    // 2. 총 재생횟수 기준 장르 내림차순 정렬
+    const sortedGenreSum = Object.keys(genresPlaySum).sort((a,b) => {
+        return genresPlaySum[b] - genresPlaySum[a];
     })
-    
-    for (genre of sortedGenres) {
+    // 3. sortedGenreSum 순회
+    for (const genre of sortedGenreSum) {
         const filteredSongs = songs.filter((song) => {
-            return genre === song.genre;
+            return song.genre == genre;
         })
-        const sortedFilterdSongs = filteredSongs.sort((a,b) => {
-            return b.play - a.play;
-        })
-        console.log(sortedFilterdSongs);
+        filteredSongs.sort((a,b) => {
+            if (a.play != b.play) {
+                return b.play - a.play;
+            } 
+            return a.id - b.id;
+        });
         
         for (let i = 0; i < 2; i++) {
-            if (sortedFilterdSongs[i]) {
-                answer.push(sortedFilterdSongs[i].id);    
+            if (filteredSongs[i]) {
+                answer.push(filteredSongs[i].id);    
             }
         }
     }

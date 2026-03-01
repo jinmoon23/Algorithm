b21122b82a9c7c2cd7733acfff19b2013bfb5f51
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2026-03-01 11:29:42 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 26.52 ms, Memory: 48.9 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.js
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.js
@@ -1,30 +1,31 @@
+// 추천인에게 배분 먼저, 나머지는 자신이 가짐
 function solution(enroll, referral, seller, amount) {
     var answer = [];
-    // 1. referral과 enroll로 단계 매핑
+    // 1. 추천인과 가입자 연결
     const relation = {};
     for (let i = 0; i < referral.length; i++) {
-        const parent = referral[i];
-        const child = enroll[i];
-        relation[child] = parent;
+        relation[enroll[i]] = referral[i];
     }
-    // 2. enroll 객체 금액 초기화
+    // 2. 각 인원이 가지는 금액 초기화
     const total = {};
     for (let i = 0; i < enroll.length; i++) {
-        const name = enroll[i];
-        total[name] = 0;
+        total[enroll[i]] = 0;
     }
-    // 3. 금액 배분
+    // 3. 배분
     for (let i = 0; i < seller.length; i++) {
-        let profit = amount[i] * 100
-        let sender = seller[i];
+        // 3-1. 각 seller의 금액(이후 재할당되므로 let 선언)
+        let money = amount[i] * 100;
+        // 3-2. 현재의 seller 정의
+        let currSender = seller[i];
         
-        while (profit > 0 && sender != "-") {
-            const receiver = relation[sender];
-            const share = Math.floor(profit / 10);
-            total[sender] += profit - share;
+        // 3-3. 말단에서부터 최상단까지 배분
+        while(money > 0 && currSender != "-") {
+            const nextSender = relation[currSender];
+            const share = Math.floor(money / 10);
+            total[currSender] += money - share;
             
-            sender = receiver;
-            profit = share;
+            money = share;
+            currSender = nextSender;
         }
     }
     

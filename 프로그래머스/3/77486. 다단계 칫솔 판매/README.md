cb066a974953d7283e253e2bed66abe6ca8c5ef6
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2026-03-01 11:45:21 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 64.97 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/README.md
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/README.md
@@ -1,10 +1,10 @@
 # [level 3] 다단계 칫솔 판매 - 77486 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77486?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77486?language=java) 
 
 ### 성능 요약
 
-메모리: 48.9 MB, 시간: 26.52 ms
+메모리: 123 MB, 시간: 64.97 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 01일 11:29:40
+2026년 03월 01일 11:45:19
 
 ### 문제 설명
 
--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -4,33 +4,34 @@ import java.util.HashMap;
 class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
         int[] answer = new int[enroll.length];
-        // 1. 상하관계 자료구조 설정
-        HashMap<String, String> relation = new HashMap<>();
-        HashMap<String, Integer> total = new HashMap<>();
         
-        System.out.println(relation);
+        HashMap<String, String> relation = new HashMap<>();
         for (int i = 0; i < enroll.length; i++) {
             relation.put(enroll[i], referral[i]);
-            total.put(enroll[i], 0);    
-        }
-        // 2. 분배
-        for (int i = 0; i < seller.length; i++) {
-            int money = amount[i] * 100;
-            String sender = seller[i];
-            
-            while (!sender.equals("-") && money > 0) {
-                int share = money / 10;
-                String receiver = relation.get(sender);
-                total.put(sender, total.get(sender) + (money - share));
-                
-                sender = receiver;
-                money = share;
-            }
         }
         
+        HashMap<String, Integer> total = new HashMap<>();
+        for (int i = 0; i < enroll.length; i++) {
+            total.put(enroll[i], 0);
+        }
+        
+        for (int i = 0; i < seller.length; i++) {
+            int money = amount[i] * 100;
+            String currSender = seller[i];
+            
+            while (!currSender.equals("-") && money > 0) {
+                int share = money / 10;
+                String nextSender = relation.get(currSender);
+                total.put(currSender, total.get(currSender) + (money - share));
+                
+                money = share;
+                currSender = nextSender;
+            }
+        }
         for (int i = 0; i < enroll.length; i++) {
             answer[i] = total.get(enroll[i]);
         }
+        
         return answer;
     }
 }

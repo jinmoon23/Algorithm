fac6c4df8142d751c39a4b1e345ac121509e369c
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2026-03-03 08:00:11 +0900

    [level 2] Title: 미로 탈출, Time: 9.11 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,26 +1,26 @@
-import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.Arrays;
+import java.util.ArrayDeque;
 
 class Solution {
-    public int bfs(String[] maps, int row, int col, int si, int sj, char target) {
-        int[][] delta = {{0,-1}, {-1,0}, {0,1}, {1,0}}; // 좌상우하
+    public int bfs(String[] maps, int si, int sj, char target, int row, int col) {
+        int[][] dxy = {{0,-1}, {-1,0}, {0,1}, {1,0}}; //좌상우하
         int[][] visited = new int[row][col];
         Deque<int[]> q = new ArrayDeque<>();
+        
         q.addLast(new int[]{si, sj, 0});
         visited[si][sj] = 1;
         
         while (!q.isEmpty()) {
-            int[] curr = q.pollFirst();
-            int i = curr[0], j = curr[1], d = curr[2];
+            int[] leftPopped = q.pollFirst();
+            int i = leftPopped[0], j = leftPopped[1], d = leftPopped[2];
             
             if (maps[i].charAt(j) == target) return d;
             
-            for (int[] dir : delta) {
+            for (int[] dir : dxy) {
                 int nx = i + dir[0], ny = j + dir[1];
                 if (nx < 0 || ny < 0 || nx >= row || ny >= col) continue;
-                if (visited[nx][ny] == 1) continue;
                 if (maps[nx].charAt(ny) == 'X') continue;
+                if (visited[nx][ny] == 1) continue;
                 
                 visited[nx][ny] = 1;
                 q.addLast(new int[]{nx, ny, d + 1});
@@ -29,7 +29,9 @@ class Solution {
         return -1;
     }
     public int solution(String[] maps) {
-        int row = maps.length, col = maps[0].length();
+        int answer = 0;
+        int row = maps.length;
+        int col = maps[0].length();
         int si = -1, sj = -1, li = -1, lj = -1;
         
         for (int i = 0; i < row; i++) {
@@ -44,9 +46,10 @@ class Solution {
                 }
             }
         }
-        int d1 = bfs(maps, row, col, si, sj, 'L');
+        
+        int d1 = bfs(maps, si, sj, 'L', row, col);
         if (d1 == -1) return -1;
-        int d2 = bfs(maps, row, col, li, lj, 'E');
+        int d2 = bfs(maps, li, lj, 'E', row, col);
         if (d2 == -1) return -1;
         
         return d1 + d2;

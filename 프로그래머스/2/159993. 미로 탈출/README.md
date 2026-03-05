3764ee0c0586036cb22bb7024d51383b0f86d9c0
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2026-03-06 07:57:54 +0900

    [level 2] Title: 미로 탈출, Time: 40.09 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/README.md
+++ 프로그래머스/2/159993. 미로 탈출/README.md
@@ -1,10 +1,10 @@
 # [level 2] 미로 탈출 - 159993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993?language=javascript) 
 
 ### 성능 요약
 
-메모리: 9.36 MB, 시간: 12.97 ms
+메모리: 38.7 MB, 시간: 40.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 03일 08:11:59
+2026년 03월 06일 07:57:51
 
 ### 문제 설명
 
--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.js
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.js
@@ -1,34 +1,44 @@
-// Start -> Lever -> End
+// 출발 지점에서 먼저 레버가 있는 칸으로 이동하여 레버를 당긴 후 미로를 빠져나가는 문이 있는 칸으로 이동하면 됩니다.
+// 최대한 빠르게 미로를 빠져나가는데 걸리는 시간을 구하려 합니다. -> BFS
 function solution(maps) {
     var answer = 0;
-    const [row, col] = [maps.length, maps[0].length];
-    let si = -1, sj = -1 , li = -1, lj = -1;
+    const n = maps.length, m = maps[0].length;
+    // 1. 각 시작 지점별 좌표 초기화
+    let si = -1, sj = -1, li = -1, lj = -1;
     
-    for (let i = 0; i < row; i++) {
-        for (let j = 0; j < col; j++) {
-            if (maps[i][j] === "S") si = i, sj = j;
-            if (maps[i][j] === "L") li = i, lj = j;
+    // 2. maps 중첩 배열 순회하며 각 시작 지점별 좌표 찾아 재할당
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < m; j++) {
+            if (maps[i][j] === 'S') {
+                si = i; 
+                sj = j;   
+            }
+            if (maps[i][j] === 'L') {
+                li = i;
+                lj = j;   
+            }
         }
     }
+    //3. bfs 탐색 함수 정의
     function bfs(si, sj, target) {
-        // const visited = new Array(row).fill(0).map((_) => new Array(col).fill(0));
-        const visited = Array.from({ length: row }, () => Array(col).fill(0));
-        console.log(visited);
+        const visited = Array.from({ length : n }, () => Array(m).fill(0));
+        const delta = [[0,-1], [-1,0], [0,1], [1,0]] // 좌상우하
         const q = [];
-        const dxy = [[0,-1], [-1,0], [0,1], [1,0]] // 좌상우하
-        q.push([si, sj, 0]);
+        q.push([si, sj, 0]); // 0은 길이
         visited[si][sj] = 1;
         
-        while (q.length) {
+        while(q.length) {
             const [i, j, d] = q.shift();
-            
+            // target 찾으면 길이 반환
             if (maps[i][j] === target) return d;
             
-            for (const [dx, dy] of dxy) {
+            // delta 순회하며 인접 격자칸 q에 담기
+            for (const [dx, dy] of delta) {
                 const nx = i + dx, ny = j + dy;
-                if (nx < 0 || ny < 0 || nx >= row || ny >= col) continue;
-                if (maps[nx][ny] === "X") continue;
-                if (visited[nx][ny] === 1) continue;
+                
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                if (maps[nx][ny] === 'X') continue;
+                if (visited[nx][ny]) continue;
                 
                 visited[nx][ny] = 1;
                 q.push([nx, ny, d + 1]);
@@ -36,10 +46,9 @@ function solution(maps) {
         }
         return -1;
     }
-    
-    const d1 = bfs(si, sj, "L");
+    let d1 = bfs(si, sj, 'L');
     if (d1 === -1) return -1;
-    const d2 = bfs(li, lj, "E");
+    let d2 = bfs(li, lj, 'E');
     if (d2 === -1) return -1;
     
     return d1 + d2;

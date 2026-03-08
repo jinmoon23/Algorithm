1c1e399e6c2dd2d0504c0edb31a0620ff0b47afd
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2026-03-08 16:59:40 +0900

    [level 3] Title: 양과 늑대, Time: 7.76 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -4,28 +4,26 @@ def solution(info, edges):
     answer = 0
     n = len(info)
     relation = [[] for _ in range(n)]
-    for p,c in edges:
-        relation[p].append(c)
-        
+    for p, c in edges: relation[p].append(c)
+    
     q = deque()
     q.append([0, 1, 0, set()])
     
     while q:
-        current, sheep_count, wolf_count, candidates = q.popleft()
+        curr_node, sheep_count, wolf_count, candidates = q.popleft()
         
         answer = max(answer, sheep_count)
         
-        for c in relation[current]:
-            candidates.add(c)
-            
-        for c in candidates:
-            if (info[c]):
-                if (sheep_count > wolf_count + 1):
+        for candidate in relation[curr_node]: candidates.add(candidate)
+        
+        for next_node in candidates:
+            if info[next_node]:
+                if sheep_count > wolf_count + 1:
                     new_candidates = set(candidates)
-                    new_candidates.remove(c)
-                    q.append([c, sheep_count, wolf_count + 1, new_candidates])
+                    new_candidates.remove(next_node)
+                    q.append([next_node, sheep_count, wolf_count + 1, new_candidates])
             else:
                 new_candidates = set(candidates)
-                new_candidates.remove(c)
-                q.append([c, sheep_count + 1, wolf_count, new_candidates])
+                new_candidates.remove(next_node)
+                q.append([next_node, sheep_count + 1, wolf_count, new_candidates])
     return answer

eb51fe3b45f0536481d8b63a818ed610891d7fe7
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2026-03-10 07:30:54 +0900

    [level 3] Title: 길 찾기 게임, Time: 37.98 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/README.md
+++ 프로그래머스/3/42892. 길 찾기 게임/README.md
@@ -1,10 +1,10 @@
 # [level 3] 길 찾기 게임 - 42892 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42892?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42892?language=java) 
 
 ### 성능 요약
 
-메모리: 43 MB, 시간: 190.09 ms
+메모리: 88.7 MB, 시간: 37.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 10일 07:02:54
+2026년 03월 10일 07:30:52
 
 ### 문제 설명
 
--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -2,11 +2,11 @@ import java.util.List;
 import java.util.ArrayList;
 
 class Node {
-    int n, x, y;
+    int num, x, y;
     Node left, right;
     
-    Node(int n, int x, int y) {
-        this.n = n;
+    Node(int num, int x, int y) {
+        this.num = num;
         this.x = x;
         this.y = y;
         this.left = null;
@@ -14,21 +14,22 @@ class Node {
     }
 }
 class Solution {
-    public Node insert(Node root, int x, int y, int n) {
-        if (root == null) return new Node(n, x, y);
+    public Node insert(Node root, int num, int x, int y) {
+        if (root == null) return new Node(num, x, y);
         if (x < root.x) {
-            root.left = insert(root.left, x, y, n);
+            root.left = insert(root.left, num, x, y);
         } else if (x > root.x) {
-            root.right = insert(root.right, x, y, n);
+            root.right = insert(root.right, num, x, y);
         }
         return root;
     }
     public void preorder(Node node, List<Integer> answer) {
         if (node == null) return;
         
-        answer.add(node.n);
+        answer.add(node.num);
         preorder(node.left, answer);
         preorder(node.right, answer);
+        
     }
     
     public void postorder(Node node, List<Integer> answer) {
@@ -36,31 +37,32 @@ class Solution {
         
         postorder(node.left, answer);
         postorder(node.right, answer);
-        answer.add(node.n);
+        answer.add(node.num);
     }
     public List<List<Integer>> solution(int[][] nodeinfo) {
         List<List<Integer>> answer = new ArrayList<>();
-        for (int i = 0; i < 2; i++) answer.add(new ArrayList<>());
-        
         int n = nodeinfo.length;
+        for (int i = 0; i < 2; i++) answer.add(new ArrayList<>());
+        // 1. y 좌표 기준 내림차순, x 좌표 기준 오름차순 정렬
         List<Integer> nodes = new ArrayList<>();
-        
         for (int i = 0; i < n; i++) nodes.add(i + 1);
         nodes.sort((a, b) -> {
-            int[] nodeA = nodeinfo[a - 1];
-            int[] nodeB = nodeinfo[b - 1];
-            if (nodeA[1] == nodeB[1]) return Integer.compare(nodeA[0], nodeB[0]);
-            return Integer.compare(nodeB[1], nodeA[1]);
+            int[] axy = nodeinfo[a - 1], bxy = nodeinfo[b - 1];
+            int ax = axy[0], ay = axy[1], bx = bxy[0], by = bxy[1];
+            if (ay == by) return Integer.compare(ax, bx);
+            else return Integer.compare(by, ay);
         });
         
+        // 2. BT 삽입
         Node root = null;
-        for (int no : nodes) {
-            int[] node = nodeinfo[no - 1];
-            int x = node[0], y = node[1];
-            root = insert(root, x, y, no);
+        for (int node : nodes) {
+            int[] coor = nodeinfo[node - 1];
+            int x = coor[0], y = coor[1];
+            root = insert(root, node, x, y);
         }
+        // 순회
         preorder(root, answer.get(0));
-        postorder(root, answer.get(1));
+        postorder(root, answer.get(1));    
         return answer;
     }
 }

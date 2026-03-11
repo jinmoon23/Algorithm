a7956be5274e0363af43e9633451ac7b02d2664c
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2026-03-12 07:15:15 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,24 +1,22 @@
--- 코드를 작성해주세요
--- FRONTEND 필터링
 SELECT GRADE, ID, EMAIL
-FROM (SELECT 
-        CASE WHEN SKILL_CODE & (SELECT SUM(CODE) AS FRONT_END_SKILLS
-                            FROM SKILLCODES
-                            WHERE CATEGORY = 'Front End') != 0 
-                            AND SKILL_CODE & (SELECT CODE
-                                            FROM SKILLCODES
-                                            WHERE NAME = 'Python') != 0 THEN 'A'
-            WHEN SKILL_CODE & (SELECT CODE
-                            FROM SKILLCODES 
-                            WHERE NAME = 'C#') != 0 THEN 'B'
-            WHEN SKILL_CODE & (SELECT SUM(CODE) AS FRONT_END_SKILLS
-                            FROM SKILLCODES
-                            WHERE CATEGORY = 'Front End') != 0  THEN 'C'
+FROM (SELECT
+    CASE WHEN SKILL_CODE & (SELECT SUM(CODE) AS FRONT_SKILLS
+                           FROM SKILLCODES
+                           WHERE CATEGORY = 'Front End') AND SKILL_CODE & (SELECT CODE
+                                                                          FROM SKILLCODES
+                                                                          WHERE NAME = 'Python')
+        THEN 'A'
+        WHEN SKILL_CODE & (SELECT CODE
+                          FROM SKILLCODES
+                          WHERE NAME = 'C#') THEN 'B'
+        WHEN SKILL_CODE & (SELECT SUM(CODE) AS FRONT_SKILLS
+                          FROM SKILLCODES
+                          WHERE CATEGORY = 'Front End') THEN 'C'
         END AS GRADE, ID, EMAIL
-    FROM DEVELOPERS) T
-WHERE GRADE IS NOT NULL
-ORDER BY GRADE, ID;
-    
-
-
+FROM
+    DEVELOPERS) T
+WHERE
+    GRADE IS NOT NULL
+ORDER BY
+    GRADE, ID;
     

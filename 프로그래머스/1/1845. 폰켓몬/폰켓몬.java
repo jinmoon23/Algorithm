39d2e4bd068f7dc8f299514fe86bc2132a2e43d3
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2026-03-12 07:00:17 +0900

    [level 1] Title: 폰켓몬, Time: 5.80 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -3,12 +3,9 @@ import java.util.HashSet;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
         int n = nums.length / 2;
         Set<Integer> sets = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            sets.add(nums[i]);
-        }
-        return Math.min(sets.size(), n);
+        for (int num : nums) sets.add(num);
+        return Math.min(n, sets.size());
     }
 }

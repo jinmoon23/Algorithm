0dc2390dc961e9e23b79c4b344ed38607c2bb01c
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2026-03-13 11:00:40 +0900

    [level 2] Title: 전화번호 목록, Time: 97.74 ms, Memory: 29.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=python3) 
 
 ### 성능 요약
 
-메모리: 97.6 MB, 시간: 376.48 ms
+메모리: 29.8 MB, 시간: 97.74 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 13일 10:57:18
+2026년 03월 13일 11:00:38
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,10 +1,6 @@
 def solution(phone_book):
-    phone_dict = {phone: 1 for phone in phone_book}  # 모든 번호를 해시에 저장
-
-    for phone in phone_book:
-        prefix = ""
-        for char in phone[:-1]:  # 마지막 글자 전까지 접두어 생성
-            prefix += char
-            if prefix in phone_dict:
-                return False  # 접두어가 해시에 존재하면 false 반환
+    phone_book.sort()
+    for i in range(len(phone_book) - 1):
+        currPhone, postPhone = phone_book[i], phone_book[i + 1]
+        if postPhone.startswith(currPhone): return False
     return True

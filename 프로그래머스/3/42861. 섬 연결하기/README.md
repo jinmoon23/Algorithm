6d503e1d99640004069c45f3081fffa85691137b
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2026-03-19 07:07:04 +0900

    [level 3] Title: 섬 연결하기, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861?language=javascript) 
 
 ### 성능 요약
 
-메모리: 9.34 MB, 시간: 0.09 ms
+메모리: 33.5 MB, 시간: 0.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 18일 10:07:57
+2026년 03월 19일 07:07:01
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
@@ -1,44 +1,43 @@
-function find(parent, x) {
-    if (x === parent[x]) return x;
+function find(parent, i) {
+    if (i === parent[i]) return i;
     
-    parent[x] = find(parent, parent[x]);
+    parent[i] = find(parent, parent[i]);
     
-    return parent[x];
+    return parent[i];
 }
 
 function union(parent, rank, xroot, yroot) {
-    // 랭크가 더 큰 루트로 합치기
     if (rank[xroot] > rank[yroot]) {
         parent[yroot] = xroot;
     } else if (rank[yroot] > rank[xroot]) {
         parent[xroot] = yroot;
-    // 랭크가 같으면 임의의 노드를 루트로 합치기
     } else {
-        parent[yroot] = xroot;
-        rank[xroot] += 1;
+        parent[yroot] = xroot; 
     }
 }
+
 function solution(n, costs) {
-    // 1. 최소 비용을 위해 costs 오름차순 정렬
+    // 1. 최소 비용으로 연결하기 위해 비용 기준 sort
     costs.sort((a,b) => a[2] - b[2]);
-    // 2. parent와 rank 초기화
-    const parent = Array.from({length:n}, (_, i) => i);
+    
+    // 2. 각 노드의 부모 노드의 번호를 담은 배열 parent 초기화
+    const parent = Array.from({ length : n }, (_, i) => i);
+    // 3. 각 노드의 union 우선순위를 담은 배열 rank 초기화
     const rank = Array(n).fill(0);
     
-    // 반환값
-    let minCost = 0;
-    // edges가 n - 1 이 될 때까지 순회할 예정
-    let edges = 0;
+    let minCost = 0
+    let edges = 0
     
     for (const edge of costs) {
+        // 4. 간선의 개수가 n-1이 되면 모든 노드가 연결된 상태라 순회를 중단한다
         if (edges === n - 1) break;
-        // 각 노드의 루트 찾기
-        const x = find(parent, edge[0]);
-        const y = find(parent, edge[1]);
-        // 각 노드의 루트가 서로 다르면 연결되어 있지 않음을 의미
-        // 루트 기준 결합(union)해서 노드 연결
-        if (x !== y) {
-            union(parent, rank, x, y);
+        // 5. 간선의 루트를 찾는다 -> 이후 연결해도 되는 노드들인지 확인하는데 활용함
+        const xroot = find(parent, edge[0]);
+        const yroot = find(parent, edge[1]);
+        
+        // 6. 서로 다른 루트라면 서로 연결되어 있지 않음을 의미. 연결하자
+        if (xroot !== yroot) {
+            union(parent, rank, xroot, yroot);
             minCost += edge[2];
             edges += 1;
         }

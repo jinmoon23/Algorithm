6b482e29b72767c19a50dbace242fcf4e70b870c
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2026-03-19 07:23:55 +0900

    [level 3] Title: 섬 연결하기, Time: 0.04 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861?language=python3) 
 
 ### 성능 요약
 
-메모리: 80.4 MB, 시간: 0.71 ms
+메모리: 9.26 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 19일 07:16:13
+2026년 03월 19일 07:23:52
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,29 +1,30 @@
-def find(parent, x):
-    if x == parent[x]: return x
+def find(parent, i):
+    if i == parent[i]: return i
     
-    parent[x] = find(parent, parent[x])
-    return parent[x]
+    parent[i] = find(parent, parent[i])
+    
+    return parent[i]
 
 def union(parent, rank, xroot, yroot):
-    if rank[xroot] > rank[yroot]:
-        parent[yroot] = xroot
-    elif rank[yroot] > rank[xroot]:
-        parent[xroot] = yroot
-    else:
-        parent[yroot] = xroot
-        rank[xroot] += 1
+    if (rank[xroot] > rank[yroot]): parent[yroot] = xroot
+    elif (rank[yroot] > rank[xroot]): parent[xroot] = yroot
+    else: parent[yroot] = xroot
+    
     
 def solution(n, costs):
     costs.sort(key = lambda x: x[2])
     
     parent, rank = [i for i in range(n)], [0 for _ in range(n)]
+    
     minCost, edges = 0, 0
     
     for edge in costs:
-        xroot, yroot = find(parent, edge[0]), find(parent, edge[1])
+        if edges == n - 1: break
         
+        xroot, yroot = find(parent, edge[0]), find(parent, edge[1])
         if xroot != yroot:
             union(parent, rank, xroot, yroot)
+            
             minCost += edge[2]
             edges += 1
     

44bc48fb1dc3f75a6446c181f04097eacbcbd9a6
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2026-03-19 22:46:40 +0900

    [level 3] Title: 양과 늑대, Time: 17.11 ms, Memory: 39.9 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/README.md
+++ 프로그래머스/3/92343. 양과 늑대/README.md
@@ -1,10 +1,10 @@
 # [level 3] 양과 늑대 - 92343 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343?language=javascript) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 7.76 ms
+메모리: 39.9 MB, 시간: 17.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 08일 16:59:38
+2026년 03월 19일 22:46:37
 
 ### 문제 설명
 
--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.js
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.js
@@ -1,41 +1,35 @@
 function solution(info, edges) {
-    var answer = 0;
-    // 1. info 길이를 갖는 트리 edges 활용해서 생성
     const n = info.length;
-    const relation = Array.from({ length : n }, () => [])
-    for (const [p, c] of edges) {
+    const relation = Array.from({ length : n }, () => []);
+    for (let i = 0; i < edges.length; i++) {
+        const p = edges[i][0], c = edges[i][1];
         relation[p].push(c);
     }
-    // 2. 인접 노드 탐색을 위한 큐 생성
+    let maxSheepCount = 0;
     const q = [];
-    q.push([0, 1, 0, new Set()]); // 현재 노드, 양의 수, 늑대의 수, 이동 가능 후보 집합
+    q.push([0,1,0, new Set()]) // 시작 노드, 양의 수, 늑대의 수, 후보노드집합
     
-    // 3. bfs 탐색
-    while (q.length) {
-        const [current, sheepCount, wolfCount, candidates] = q.shift();
-        answer = Math.max(answer, sheepCount);
+    while (q.length > 0) {
+        const [start, sheepCount, wolfCount, candidates] = q.shift();
+        maxSheepCount = Math.max(maxSheepCount, sheepCount);
         
-        // 4. candidates 집합에 후보 노드 삽입
-        // current 변수를 활용해 relation[current]로 이동 가능 후보 노드에 접근
-        for (const candidate of relation[current]) {
-            candidates.add(candidate);
-        }
-        // 5. candidates 집합 순회하며 양과 늑대 판단
+        const nextNodes = relation[start];
+        for (const node of nextNodes) candidates.add(node);
         for (const next of candidates) {
-            // 늑대인 경우
+            // 후보 노드의 값이 1(늑대)이면
             if (info[next]) {
                 if (sheepCount > wolfCount + 1) {
                     const newCandidates = new Set(candidates);
                     newCandidates.delete(next);
                     q.push([next, sheepCount, wolfCount + 1, newCandidates]);
                 }
-            // 양인 경우
             } else {
                 const newCandidates = new Set(candidates);
                 newCandidates.delete(next);
                 q.push([next, sheepCount + 1, wolfCount, newCandidates]);
             }
         }
+        
     }
-    return answer;
+    return maxSheepCount;
 }

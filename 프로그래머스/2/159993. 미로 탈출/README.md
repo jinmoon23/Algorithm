1cfc45960899e0eb0e4ce32b485e66acaed6400b
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2026-03-19 22:11:19 +0900

    [level 2] Title: 미로 탈출, Time: 17.13 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/README.md
+++ 프로그래머스/2/159993. 미로 탈출/README.md
@@ -1,10 +1,10 @@
 # [level 2] 미로 탈출 - 159993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993?language=python3) 
 
 ### 성능 요약
 
-메모리: 92.1 MB, 시간: 8.92 ms
+메모리: 9.17 MB, 시간: 17.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 19일 22:01:29
+2026년 03월 19일 22:11:17
 
 ### 문제 설명
 
--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -1,38 +1,33 @@
 from collections import deque
 
-
 def solution(maps):
     n, m = len(maps), len(maps[0])
-    si, sj, ni, nj = -1, -1, -1, -1
+    si, sj, li, lj = -1, -1, -1, -1
     for i in range(n):
         for j in range(m):
-            if maps[i][j] == 'S': si, sj = i, j
-            if maps[i][j] == 'L': li, lj = i, j
+            if (maps[i][j] == 'S'): si, sj = i, j
+            elif (maps[i][j] == 'L'): li, lj = i, j
     
-    def bfs(si, sj, target):
-        delta = [[0,-1], [-1,0], [0,1], [1,0]] # 좌상우하
+    def bfs(i, j, target):
         visited = [[0 for _ in range(m)] for _ in range(n)]
+        dxy = [[0,-1], [-1,0], [0,1], [1,0]] # 좌상우하
         q = deque()
-        q.append([si, sj, 0])
+        q.append([i,j,0])
         
-        while q:
-            i, j, d = q.popleft()
+        while(q):
+            x, y, c = q.popleft()
+            if (maps[x][y] == target): return c
             
-            if maps[i][j] == target: return d
-            
-            for dx, dy in delta:
-                nx, ny = i + dx, j + dy
-                
+            for dx, dy in dxy:
+                nx, ny = x + dx, y + dy
                 if nx < 0 or ny < 0 or nx >= n or ny >= m: continue
+                if visited[nx][ny] == 1: continue
                 if maps[nx][ny] == 'X': continue
-                if visited[nx][ny]: continue
                 
+                q.append([nx, ny, c + 1])
                 visited[nx][ny] = 1
-                q.append([nx, ny, d + 1])
         return -1
-    
-    d1, d2 = bfs(si, sj, 'L'), bfs(li, lj, 'E')
-    if d1 == -1: return -1
-    if d2 == -1: return -1
-    
-    return d1 + d2
+    c1, c2 = bfs(si, sj, 'L'), bfs(li, lj, 'E')
+    if (c1 == -1): return -1
+    if (c2 == -1): return -1
+    return c1 + c2

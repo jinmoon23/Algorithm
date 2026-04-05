9d1f8a037ca54cefbbf245b1f22c6b5db62655df
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2026-04-05 17:25:42 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 36.67 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=javascript) 
 
 ### 성능 요약
 
-메모리: 9.4 MB, 시간: 10.38 ms
+메모리: 38.2 MB, 시간: 36.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 02일 23:17:23
+2026년 04월 05일 17:25:38
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,28 +1,30 @@
 function solution(maps) {
     const n = maps.length, m = maps[0].length;
     
-    function bfs(i, j) {
-        const dxy = [[0,-1], [-1,0], [0,1], [1,0]]; // 좌,상,우,하
-        const q = [];
-        q.push([i, j, 1]);
-        const visited = Array.from({ length: n }, () => Array(m).fill(0));
-        visited[i][j] = 1;
+    function bfs(si, sj) {
+        const dxy = [[0,-1], [-1, 0], [0, 1], [1, 0]] // 좌상우하
+        const visited = Array.from({ length : n }, () => Array(m).fill(0));
         
-        while (q.length > 0) {
-            const [x, y, c] = q.shift();
-            if (x === n - 1 && y === m - 1) return c;
+        const q = [];
+        q.push([si, sj, 1]);
+        visited[si][sj] = 1;
+        
+        while(q.length > 0) {
+            const [i, j, cnt] = q.shift();
+            if (i === n - 1 && j === m - 1) return cnt;
             
             for (const [dx, dy] of dxy) {
-                const nx = x + dx, ny = y + dy;
+                const nx = i + dx, ny = j + dy;
                 if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
                 if (visited[nx][ny] === 1) continue;
                 if (maps[nx][ny] === 0) continue;
                 
-                q.push([nx, ny, c + 1]);
+                q.push([nx, ny, cnt + 1]);
                 visited[nx][ny] = 1;
             }
         }
-        return -1;
+        return -1;    
     }
-    return bfs(0,0);
+    const result = bfs(0, 0);
+    return result;
 }
